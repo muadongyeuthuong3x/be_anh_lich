0391dc7e3b54d5d9d0f565ff15a1f4811ab424f3
optimus_mc <muadongyeuthuong3x>
2024-10-16 09:25:43 +0700

    app

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
-const connectDB = require('./config/db');
-const router = require('./routes');
-
-const app = express();
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}));
-app.use(express.json());
-app.use(cookieParser());
-
-app.use("/api", router);
-
-// Xuất hàm serverless
 module.exports = async (req, res) => {
-    // Kết nối đến cơ sở dữ liệu mỗi lần hàm được gọi
-    await connectDB();
-
-    // Gọi middleware Express
-    app(req, res);
+    try {
+        await connectDB();
+        app(req, res);
+    } catch (error) {
+        console.error("Database connection error:", error);
+        res.status(500).send("Internal Server Error");
+    }
 };
