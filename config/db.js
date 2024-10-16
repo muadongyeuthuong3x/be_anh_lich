8d3ac70fcd120dc529ad2575d96b099d153a434e
optimus_mc <muadongyeuthuong3x>
2024-10-16 09:39:59 +0700

    app

--- config/db.js
+++ config/db.js
@@ -1,12 +1,21 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
+let dbConnection;
 
-async function connectDB(){
-    try{
-        await mongoose.connect(process.env.MONGODB_URI)
-    }catch(err){
-        console.log(err)
+async function connectDB() {
+    if (!dbConnection) {
+        try {
+            dbConnection = await mongoose.connect(process.env.MONGODB_URI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
+            console.log("Database connected");
+        } catch (err) {
+            console.error("Database connection error:", err);
+            throw err; // Ném lại lỗi để xử lý ở nơi khác
+        }
     }
+    return dbConnection;
 }
 
-module.exports = connectDB
+module.exports = connectDB;
