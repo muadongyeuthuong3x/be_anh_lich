6b0bc97bcf64b0869e67868f6fbf321a40d5d8ad
optimus_mc <muadongyeuthuong3x>
2024-10-18 13:53:46 +0700

    update

--- src/index.js
+++ src/index.js
@@ -14,6 +14,10 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
+
+app.use(express.json({ limit: '10mb' })); 
+app.use(express.urlencoded({ limit: '10mb', extended: true })); 
+
 app.use("/api",router)
 
 const PORT = 8080 || process.env.PORT
